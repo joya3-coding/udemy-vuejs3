9073c587ad58a12fdb69a2f883a0e351a04851b0
joya3-coding <kkd.douga@gmail.com>
2021-10-21 18:57:08 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/" active-class="link--active" exact class="link">Home</router-link> |
+      <router-link to="/users" active-class="link--active" exact class="link">Users</router-link>
+    </div>
+    <router-view />
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from "./views/Home.vue";
+export default {
+  components: {
+    Home
+  }
+}
+</script>
+
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.link--active {
+  font-size: 25px;
+}
+</style>
---
+++ src/views/Users.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="users">
+    <h1>Users</h1>
+    <router-link to="/users/1">ユーザー１</router-link>
+    <router-link to="/users/2">ユーザー２</router-link>
+    <hr>
+    <h1>User No. {{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  watch: {
+    $route(to, from) {
+      alert(this.id)
+      alert(`$routeがid:${from.params.id}からid:${to.params.id}に変わった。`)
+    }
+  }
+}
+</script>
+
